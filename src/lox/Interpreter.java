989eea2335bec0b061eb7e54775fd67fe3bd68bb
Aleksei Lazunin <gegcuk@gmail.com>
2024-09-03 18:55:37 +0100

    Add runtime error handling and integrate Interpreter with Lox
    - Enforce numeric operands for arithmetic and comparison operators.
    - Handle invalid operand types for the `PLUS` operator.
    - Introduce `RuntimeError` for detailed error reporting.
    - Modify `Lox` to track and handle runtime errors during interpretation.

--- src/lox/Interpreter.java
+++ src/lox/Interpreter.java
@@ -8,18 +8,23 @@ public class Interpreter implements Expr.Visitor<Object>{
 
         switch (expr.operator.type) {
             case GREATER:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left > (double) right;
             case GREATER_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left >= (double) right;
             case LESS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left < (double) right;
             case LESS_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left <= (double) right;
             case BANG_EQUAL:
                 return !isEqual(left, right);
             case EQUAL_EQUAL:
                 return isEqual(left, right);
             case MINUS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left - (double) right;
             case PLUS:
                 if (left instanceof Double && right instanceof Double){
@@ -27,9 +32,12 @@ public class Interpreter implements Expr.Visitor<Object>{
                 }
                 if (left instanceof String && right instanceof String)
                     return (String) left + (String) right;
+                throw new RuntimeError(expr.operator, "Operands must be numbers or strings.");
             case SLASH:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left / (double) right;
             case STAR:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left * (double) right;
         }
 
@@ -53,9 +61,21 @@ public class Interpreter implements Expr.Visitor<Object>{
             case BANG:
                 return !isTruthy(right);
             case MINUS:
+                checkNumberOperand(expr.operator, right);
                 return -(double)right;
         }
         return null;
+    }
+
+    private void checkNumberOperand(Token operator, Object operand) {
+        if (operand instanceof Double) return;
+        throw new RuntimeError(operator, "Operand must be a number.");
+    }
+
+    private void checkNumberOperands(Token operator, Object left, Object right) {
+        if (left instanceof Double && right instanceof Double) return;
+
+        throw new RuntimeError(operator, "Operands must be numbers.");
     }
 
     private boolean isTruthy(Object object) {
@@ -72,5 +92,27 @@ public class Interpreter implements Expr.Visitor<Object>{
 
     private Object evaluate(Expr expr) {
         return expr.accept(this);
+    }
+
+    void interpret(Expr expression){
+        try {
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch (RuntimeError error){
+            Lox.runtimeError(error);
+        }
+    }
+
+    private String stringify(Object object){
+        if (object == null) return "nil";
+
+        if(object instanceof Double) {
+            String text = object.toString();
+            if(text.endsWith(".0")){
+                text = text.substring(0, text.length()-2);
+            }
+            return text;
+        }
+        return object.toString();
     }
 }
