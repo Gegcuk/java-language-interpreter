e205bebd4cc9958e5a36f1d2e2b4fea7c53c6ad3
Aleksei Lazunin <gegcuk@gmail.com>
2024-09-04 13:45:14 +0100

    Extend Interpreter to support statement execution
    - Implemented statement execution in the `Interpreter` class by adding support for `Stmt.Visitor<Void>`:
      - Added `interpret(List<Stmt> statements)` method to execute a list of statements.
      - Implemented `execute(Stmt statement)` to handle the execution of individual statements.
    
    - Expanded the visitor methods to handle new statement types:
      - `visitExpressionStmt` evaluates an expression statement.
      - `visitPrintStmt` evaluates and prints the result of an expression.
    
    - Retained existing functionality for evaluating expressions, including type checks and error handling for binary and unary expressions.
    
    - This update enables the interpreter to process and execute statements, paving the way for more complex program execution.

--- src/lox/Interpreter.java
+++ src/lox/Interpreter.java
@@ -1,6 +1,8 @@
 package lox;
 
-public class Interpreter implements Expr.Visitor<Object>{
+import java.util.List;
+
+public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void>{
     @Override
     public Object visitBinaryExpr(Expr.Binary expr) {
         Object left = evaluate(expr.left);
@@ -94,13 +96,18 @@ public class Interpreter implements Expr.Visitor<Object>{
         return expr.accept(this);
     }
 
-    void interpret(Expr expression){
+    void interpret(List<Stmt> statements){
         try {
-            Object value = evaluate(expression);
-            System.out.println(stringify(value));
+            for (Stmt statement : statements) {
+                execute(statement);
+            }
         } catch (RuntimeError error){
-            Lox.runtimeError(error);
+                Lox.runtimeError(error);
         }
+    }
+
+    private void execute(Stmt statement) {
+        statement.accept(this);
     }
 
     private String stringify(Object object){
@@ -114,5 +121,18 @@ public class Interpreter implements Expr.Visitor<Object>{
             return text;
         }
         return object.toString();
+    }
+
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt) {
+        evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitPrintStmt(Stmt.Print stmt) {
+        Object value = evaluate(stmt.expression);
+        System.out.println(stringify(value));
+        return null;
     }
 }
